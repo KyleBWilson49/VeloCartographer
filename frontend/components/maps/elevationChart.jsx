bfc5cd95812ebfc321fb20e93175fe69a894dabc
KyleBWilson49 <KyleBWilson49@gmail.com>
2015-12-18 17:27:12 -0800

    working with test chart

--- frontend/components/maps/elevationChart.jsx
+++ frontend/components/maps/elevationChart.jsx
@@ -1,10 +1,37 @@
-var React = require('react');
+var React = require('react'),
+    LineChart = require('react-chartjs').Line;
 
 var ElevationChart = React.createClass({
   render: function () {
+    var chartData = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+      ]
+    };
+
     return (
-      <div>
-        this is where the chart goes
+      <div id="elevation-chart">
+        <LineChart data={chartData} width="600" height="150" />
       </div>
     );
   }
